9f9c81876dd8d253a019158fadf169c28f80bf02
Mantej Rajpal <mantej@squareup.com>
2022-10-18 00:14:46 -0400

    completed scoring function

--- go/set1/3_single_byte_xor.go
+++ go/set1/3_single_byte_xor.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	target := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 	hex_string, result := "", ""
-	var score float32
+	var score float64
 
 	for i := 0; ; i++ {
 		hex_string = strconv.FormatInt(int64(i), 16)
@@ -21,6 +21,7 @@ func main() {
 		// extend single-byte to the length of the target hex_string string
 		hex_string = extend(hex_string, target)
 
+		// hex strings that we xor should be the same length
 		if len(hex_string) != len(target) {
 			panic("hex_string strings are not the same length!")
 		} else {
@@ -30,12 +31,13 @@ func main() {
 		score = Score(result)
 
 		// high score = english
-		if score > 0.95 {
+		if score > 0.85 {
 			byteArray, err := hex.DecodeString(result)
 			if err != nil {
 				fmt.Println("Unable to convert hex to byte")
 			}
-			fmt.Println(byteArray)
+			// print something human readable instead of an array of bytes
+			fmt.Println(string(byteArray))
 		}
 
 		if i == 255 {
@@ -44,11 +46,28 @@ func main() {
 	}
 }
 
-func Score(string hex) float32 {
+// Score returns the ratio of characters in the english alphabet (and spaces) to total length of the string
+func Score(hexstring string) float64 {
+	bytes, err := hex.DecodeString(hexstring)
+	if err != nil {
+		fmt.Println("Unable to convert hex to byte")
+	}
 
-	return 0.0
+	length := float64(len(bytes))
+	var count int
+	var letter rune
+
+	for _, b := range bytes {
+		letter = rune(b)
+		if letter >= 'a' && letter <= 'z' || letter >= 'A' && letter <= 'Z' || letter == ' ' {
+			count = count + 1
+		}
+	}
+
+	return float64(count) / length
 }
 
+// extend a single-byte hex string to target string's length
 func extend(x, target string) string {
 	multiplier := len(target) / len(x)
 	return strings.Repeat(x, multiplier)
