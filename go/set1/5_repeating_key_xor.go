9a11c85a9c20ecca46a143c0a80bb777286fc1d4
Mantej Rajpal <mantej@squareup.com>
2022-11-15 18:06:28 -0500

    refactored repeating key xor challenge

--- go/set1/5_repeating_key_xor.go
+++ go/set1/5_repeating_key_xor.go
@@ -4,13 +4,14 @@ import (
 	"encoding/hex"
 )
 
-func Challenge5(input, key string) string {
+func Repeating_Key_Xor(input, key string) string {
 	input_hex := hex.EncodeToString([]byte(input))
 	key_hex := hex.EncodeToString([]byte(key))
 
 	// extend key to be of the same length of input
 	key_hex = extend(key_hex, input_hex)
 
+	// returns hex-encoded XOR of the two hex-encoded inputs
 	result := xor(input_hex, key_hex)
 
 	return result
